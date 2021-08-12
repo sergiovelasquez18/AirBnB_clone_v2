7f7132455cddd62ec444382f9148159d58937c91
mareizaa <marce.3210@gmail.com>
2021-08-12 00:30:53 +0000

    basemodel

--- models/state.py
+++ models/state.py
@@ -1,8 +1,27 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.city import City
+from os import getenv
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', backref='State', cascade='all, delete')
+
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            """getter attribute cities that returns the list of
+            City instances with state_id equals to the current State.id"""
+            from models import storage
+            new = []
+            new_dic = storage.all(City)
+            for city in new_dic.values():
+                if city.state_id == self.id:
+                    new.append(city)
+            return new

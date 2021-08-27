cac8e7000e3ef76803d3c2a422a0514252d3c04f
sergiovelasquez18 <2458@holbertonschool.com>
2021-08-26 23:19:40 -0500

    do deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """"""
-
 import os
 from fabric.api import put, run, env
 from datetime import datetime
 
+
 def do_deploy(archive_path):
-    """"""
+    '''
+    distributes an archive to your web servers, using the function do_deploy
+    '''

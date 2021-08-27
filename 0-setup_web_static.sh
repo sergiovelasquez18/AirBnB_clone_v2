f2238be1d7f538bdd1ba0e5016210a8c8ea3d592
sergiovelasquez18 <2458@holbertonschool.com>
2021-08-26 19:52:44 -0500

    setup web static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -14,5 +14,6 @@ echo "
   </body>
 </html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data/sudo sed -i '/listen 80 default_server/a location /hbnb_static/ {alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+chown -R ubuntu:ubuntu /data/
+sudo sed -i '/listen 80 default_server/a location /hbnb_static/ {alias /data/web_static/current/;}' /etc/nginx/sites-available/default
 service nginx restart

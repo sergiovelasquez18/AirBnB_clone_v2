cfd030d9be3a87d6f1e889acd385680a7fd3cd85
sergiovelasquez18 <2458@holbertonschool.com>
2021-08-26 19:21:09 -0500

    setup web static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,2 +1,19 @@
-#
-#
+# !/usr/bin/env bash
+# script that sets up your web servers for the deployment of web_static
+
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+echo "
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/current /data/web_static/releases/test/
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i '/listen 80 default_server/a location /hbnb_static/ {alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+sudo service nginx restart

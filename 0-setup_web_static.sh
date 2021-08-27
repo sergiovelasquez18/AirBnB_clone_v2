6f2e72f20ef756843371cfca1381773ef915e6c6
sergiovelasquez18 <2458@holbertonschool.com>
2021-08-26 19:48:47 -0500

    setup web static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,5 +1,5 @@
-# !/usr/bin/env bash
-# script that sets up your web servers for the deployment of web_static
+#!/usr/bin/env bash
+#script that sets up your web servers for the deployment of web_static
 
 sudo apt-get -y update
 sudo apt-get -y install nginx

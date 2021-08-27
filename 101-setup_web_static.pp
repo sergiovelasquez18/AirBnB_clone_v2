fea1415ff7f69193b844da67ca45411ef09664c5
sergiovelasquez18 <2458@holbertonschool.com>
2021-08-26 23:10:31 -0500

    setup web static

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,5 +1,5 @@
 # Puppet for setup
 exec {'web_static':
-  command  => 'sudo apt-get -y update && sudo apt-get -y install nginx && sudo service ngnix start && sudo mkdir -p /data/ && /data/web_static && /data/web_static/releases/shared/ && /data/web_static/releases/test/ && sudo echo "Holberton School" | tee /data/web_static/releases/test/index.html && ln -sf /data/web_static/releases/test/ /data/web_static/current && chown -R ubuntu:ubuntu /data/ && var="location /hbnb_static {\n alias /data/web_static/current/;\n}\n" && sudo sed -i "43i$var" /etc/nginx/sites-available/default && service nginx restart',
+  command  => 'sudo apt-get update && sudo apt-get -y install nginx && sudo service nginx start && sudo mkdir -p /data/ && sudo mkdir -p /data/web_static/ && sudo mkdir -p /data/web_static/releases/ && sudo mkdir -p /data/web_static/shared/ && sudo mkdir -p /data/web_static/releases/test/ && sudo echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html && sudo ln -sf /data/web_static/releases/test/ /data/web_static/current && sudo chown -R ubuntu: /data/ && var="location /hbnb_static {\n alias /data/web_static/current/;\n}\n" && sudo sed -i "43i$var" /etc/nginx/sites-available/default && sudo service nginx restart',
   provider => 'shell'
 }
